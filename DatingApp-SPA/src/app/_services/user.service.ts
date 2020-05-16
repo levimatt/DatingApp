e2037a2b704a35a27e008caf59760fad040dafab
levimatt <irg2000@gmail.com>
2020-05-17 01:36:23 +0200

    End of section 9

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -4,26 +4,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../_models/user';
 import { Observable } from 'rxjs';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   baseUrl = environment.apiUrl;
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-getUsers(): Observable<User[]> {
-  return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
-}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
 
-getUser(id): Observable<User>{
-  return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
-}
+  getUser(id): Observable<User>{
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
 
 }
--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery-9';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
